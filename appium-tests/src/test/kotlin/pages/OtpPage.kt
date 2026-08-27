cef92c4286ceb34cd35e7681e339db6a02d9c80f
Kirill Tomashchuk <kirill.tomashchuk@tripleten.com>
2026-08-27 14:01:16 +0200

    Second solution

--- appium-tests/src/test/kotlin/pages/OtpPage.kt
+++ appium-tests/src/test/kotlin/pages/OtpPage.kt
@@ -2,11 +2,11 @@ package pages
 
 /** Element catalog for the OTP screen. */
 object OtpPage {
-    val title = Element("otp_title")
-    val codeInput = Element("otp_input")
-    val confirmButton = Element("otp_continue_button")
-    val errorLabel = Element("otp_error")
+    val title = Element(variantId("otp_title", "auth_otp_screen_title"))
+    val codeInput = Element(variantId("otp_input", "auth_otp_code_field"))
+    val confirmButton = Element(variantId("otp_continue_button", "auth_otp_submit"))
+    val errorLabel = Element(variantId("otp_error", "auth_otp_validation_message"))
 
     /** Disabled while the 30 s cooldown ticks; its label shows the countdown. */
-    val resendButton = Element("otp_resend_button")
+    val resendButton = Element(variantId("otp_resend_button", "auth_otp_resend"))
 }
